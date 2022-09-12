8b6bd6b4a259b3feea7d408d83088e7b5403c3e6
andreyanez <andre.yanez22@gmail.com>
2022-09-12 18:57:02 -0500

    added styles to search page and HomeRow component

--- components/HomeCard.vue
+++ components/HomeCard.vue
@@ -3,27 +3,23 @@
 		<img :src="home.images[0]" style="width: 200px" alt="" />
 		<p>{{ home.title }}</p>
 		<p>
-			{{ home.location.address }}</p>
-        <p>
-            {{ home.location.city}},{{ home.location.state }}
-        </p>
+			{{ home.location.address }}
 		</p>
+		<p>{{ home.location.city }},{{ home.location.state }}</p>
 		<p>{{ home.guests }} guests</p>
 		<p>{{ home.pricePerNight }}$/night</p>
 	</div>
 </template>
 
 <script>
-    export default {
-        props:{
-            home:{
-                type: Object,
-                required: true
-            }
-        }
-    }
+export default {
+	props: {
+		home: {
+			type: Object,
+			required: true,
+		},
+	},
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- pages/search.vue
+++ pages/search.vue
@@ -1,18 +1,21 @@
 <template>
-	<div>
-		<div style="height: 800px; width: 800px; float: right" ref="map"></div>
-		Results for: {{ label }}<br />
-		<div v-if="homes.length > 0">
-			<nuxt-link v-for="home in homes" :key="home.objectID" :to="`/home/${home.objectID}`">
-				<!-- here I use .native after each event because HomeRow is a component, not a native tag -->
-				<HomeRow
-					:home="home"
-					@mouseover.native="highlightMarker(home.objectID, true)"
-					@mouseout.native="highlightMarker(home.objectID, false)"
-				/>
-			</nuxt-link>
+	<div class="app-search-results-page">
+		<div class="app-search-results">
+			<div class="app-search-results-listing">
+				<h2 class="app-title">Stays in {{ label }}</h2>
+				<nuxt-link v-for="home in homes" :key="home.objectID" :to="`/home/${home.objectID}`">
+					<HomeRow
+						class="app-house"
+						:home="home"
+						@mouseover.native="highlightMarker(home.objectID, true)"
+						@mouseout.native="highlightMarker(home.objectID, false)"
+					/>
+				</nuxt-link>
+			</div>
+			<div class="app-search-results-map">
+				<div class="app-map" ref="map"></div>
+			</div>
 		</div>
-		<div v-else>No results found</div>
 	</div>
 </template>
 
@@ -40,6 +43,7 @@ export default {
 		},
 		//in getHomeMarkers, I create an array of objects with the geoloc of each home
 		getHomeMarkers() {
+			if (this.homes.length == 0) return null;
 			return this.homes.map(home => {
 				return {
 					...home._geoloc,
